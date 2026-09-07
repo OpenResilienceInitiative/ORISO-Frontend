0ab31a99303ae7033fcb1b8ac4ab0b5915e91f2f
Frank Gerhardt <Storypapst@users.noreply.github.com>
2026-09-07 13:47:57 +0200

    fix: encode contact email as JSON string for current API

--- src/api/apiPutEmail.ts
+++ src/api/apiPutEmail.ts
@@ -5,7 +5,7 @@ export const apiPutEmail = async (email: string): Promise<any> => {
 	const url = endpoints.email;
 
 	return fetchData({
-		bodyData: email.trim(),
+		bodyData: JSON.stringify(email.trim()),
 		url: url,
 		method: FETCH_METHODS.PUT,
 		responseHandling: [FETCH_ERRORS.CONFLICT_WITH_RESPONSE]
